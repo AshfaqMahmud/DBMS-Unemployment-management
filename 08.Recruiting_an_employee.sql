88758ab5952f54f7043320d919d1193257e3eb33
Ashfaq Mahmud <84186699+AshfaqMahmud@users.noreply.github.com>
2022-07-19 23:01:29 -0700

    project done

--- 08.Recruiting_an_employee.sql
+++ 08.Recruiting_an_employee.sql
@@ -12,35 +12,52 @@ open cur;
     DBMS_OUTPUT.PUT_LINE('output '||temp.company||' '||temp.post||' '||temp.vacancy);
     END loop;
     close cur;
+    
 END;
 / 
-   
-create [or replace] procedure match_job(person_ID peopleInfo.name%type)
- is
+
+
+create or replace procedure match_job(person_ID peopleInfo.ID%type)
+ as
     temp1 peopleWorkExpertise.ID_work%type;
     person_name peopleInfo.name%type;
-    person_id peopleWorkExpertise.ID_peopleInfo%type;
     tcompany company.name%type;
     tpost work.name%type;
     tstat companyWork.status%type;
-    type row_type is table of  
 BEGIN
 select peopleInfo.name into person_name from peopleInfo where peopleInfo.ID=person_ID; --get person name
 select peopleWorkExpertise.ID_work into temp1 from peopleWorkExpertise where peopleWorkExpertise.ID_peopleInfo=person_ID;
 select distinct peopleInfo.name, work.name into person_name, tpost from peopleInfo
     join peopleWorkExpertise on peopleInfo.ID=person_ID and peopleWorkExpertise.ID_work=temp1
     join work on work.id=temp1;
-    DBMS_OUTPUT.PUT_LINE ('Person : ' || person_name || 'expert in ' || tpost || 'available companies and vacancies are-');
-    DBMS_OUTPUT.PUT_LINE( select work.name as post, company.name as company, companywork.status as vacancy from work
+    DBMS_OUTPUT.PUT_LINE ('Person : ' || person_name || ', expert in ' || tpost || ' available companies and vacancies are- ');
+    select company.name, companyWork.status into tcompany,tstat from work
     inner join company on work.id=temp1
     inner join companywork on companyWork.ID_work=temp1 and companywork.ID_company=company.ID
-    group by work.name, company.name,companywork.status;)
+    group by work.name, company.name,companywork.status;
+    DBMS_OUTPUT.PUT_LINE('Vacancy '||tstat||' Company '||tcompany);
 END match_job;
 /
 show errors;
-
+SET SERVEROUTPUT ON;
 BEGIN
     match_job(1);
 END;
 /
-   
+
+create or replace procedure add_emp(person_ID peopleInfo.ID%type,work_ID companyWork.ID_work%type,company_ID company.ID%type)
+as
+BEGIN
+INSERT INTO employeeManagement VALUES(person_id,company_ID,work_ID,sysdate);
+    update peopleInfo set status='Employed' where ID=person_ID;
+    update companyWork set status=status-1 where ID_work=work_ID;
+    DBMS_OUTPUT.PUT_LINE('Value Inserted');
+END add_emp;
+/
+show errors;
+SET SERVEROUTPUT ON;
+BEGIN
+    add_emp(1,212,107);
+END;
+/
+
